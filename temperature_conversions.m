5b0ec9d5220ff3519c641e55d1142e4e4ca6959f
Vasco Zeferina <vasco.zeferina@postgrad.manchester.ac.uk>
2018-11-07 08:41:40 +0000

    Add K-C conversion trace to graph

--- temperature_conversions.m
+++ temperature_conversions.m
@@ -10,4 +10,16 @@ fprintf('Water freezes at %g%sC, which is %g%sF.\n', fahrenheit_to_celsius(32), 
 
 fprintf('Absolute zero is 0K, which is %g%sC.\n', kelvin_to_celsius(0), deg)
 fprintf('Absolute zero is %g%sF.\n', celsius_to_fahrenheit(kelvin_to_celsius(0)), deg) 
+
+% Plot Fahrenheit vs Celsius
+degC = linspace(0,100,101);
+plot(degC, celsius_to_fahrenheit(degC))
+hold on
+degK = linspace(273, 373, 101);
+plot(kelvin_to_celsius(degK), degK)
+xlabel('Celsius')
+ylabel('Conversion')
+legend('Fahrenheit', 'Kelvin', 'location', 'northwest')
+
 end
+
